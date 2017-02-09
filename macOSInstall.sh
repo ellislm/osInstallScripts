fab4a2607b5656529424e0f3a4d46df155630c7d
loganE <logan_ellis@me.com>
2017-02-09 01:45:39 -0500

    Fixed formatting. Added things

--- macOSInstall.sh
+++ macOSInstall.sh
@@ -2,95 +2,127 @@
 #curl https://raw.githubusercontent.com/mikepurvis/ros-install-osx/master/install | bash
 #End ROS
 if [[ ! $* *--pt2* ]]
-read -n1 -rsp "Before we get started, make sure XCode is installed. Also install XQuarts, relog, and run again. Press space when it is" key
-git config --global user.email "logan_ellis@me.com"
-git config --global user.name "loganE"
+    read -n1 -rsp "Before we get started, make sure XCode is installed. Also install XQuarts, relog, and run again. Press space when it is" key
+    git config --global user.email "logan_ellis@me.com"
+    git config --global user.name "loganE"
 
-if [[ ! -f ~/.ssh/id_rsa.pub ]]; then
-ssh-keygen -t rsa -b 4096 -C "logan_ellis@me.com"
-fi
+    if [[ ! -f ~/.ssh/id_rsa.pub ]]; then
+        ssh-keygen -t rsa -b 4096 -C "logan_ellis@me.com"
+    fi
 
-read -n1 -rsp "Go to github, and paste SSH key. Press space to continue install..." key
+    echo
+    echo
+    read -n1 -rsp "Go to github, and paste SSH key. Press space to continue install..." key
 
-echo "Installing ROS Now"
-cd ~/
+    if [[ $* --ros ]]
+        echo "Installing ROS Now"
+        cd ~/
 
-if [[ ! -d ~/ros-install-osx ]]; then
-git clone http://www.github.com/loganE/ros-install-osx
-fi
-cd ros-install-osx
+        if [[ ! -d ~/ros-install-osx ]]; then
+            git clone http://www.github.com/loganE/ros-install-osx
+        fi
 
-chmod +x install && ./install
+        cd ros-install-osx
+        chmod +x install && ./install
+    else #install brew
+        /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    fi
 
-#Vimfiles
-echo 'Installing Vim'
-brew install macvim --with-lua -env-std --with-override-system-vim
-brew linkapps macvim
-brew install xz cmake
+    #Vimfiles
+    echo 'Installing Vim'
+    brew install macvim --with-lua -env-std --with-override-system-vim
+    brew linkapps macvim
+    brew install xz cmake
 
-#fzf Install
-brew install fzf
-# Install shell extensions
-/usr/local/opt/fzf/install
-echo '[ -f ~/.fzf.bash ] && source ~/.fzf.bash' >> ~/.bash_profile
+    #fzf Install
+    brew install fzf
+    # Install shell extensions
+    /usr/local/opt/fzf/install
+    echo '[ -f ~/.fzf.bash ] && source ~/.fzf.bash' >> ~/.bash_profile
 
-brew update && brew tap jlhonora/lsusb
-brew install lsusb arp-scan byobu
+    brew update && brew tap jlhonora/lsusb
+    brew install lsusb arp-scan byobu
 
-  if [ ! -d ~/vimfiles/ ]; then
-    cd ~/ && git clone http://www.github.com/logane/vimfiles
-    ln -s ~/vimfiles/ ~/.vim
-  fi
-echo "Go ahead and close iterm completely. Open a new window of iterm, then run ./macInstall.sh --pt2"
-exit 1
+    if [ ! -d ~/vimfiles/ ]; then
+        cd ~/ && git clone http://www.github.com/logane/vimfiles
+        ln -s ~/vimfiles/ ~/.vim
+    fi
+
+    echo "Go ahead and close iterm completely. Open a new window of iterm, then run ./macInstall.sh --pt2"
+    exit 1
 fi
 
 if [[ $* == *--pt2* ]]; then
-vim -c "PlugInstall" +qall
+    vim -c "PlugInstall" +qall
 
-echo 'Compiling YCM and Color Coded'
-cd ~/vimfiles/plugged/YouCompleteMe/
-./install.py --clang-complete
+    echo 'Compiling YCM and Color Coded'
+    cd ~/vimfiles/plugged/YouCompleteMe/
+    ./install.py --clang-complete
 
-cd ~/vimfiles/plugged/color_coded
-rm -rf build
-mkdir build && cd build
-cmake ..
-make && make install
+    cd ~/vimfiles/plugged/color_coded
+    rm -rf build
+    mkdir build && cd build
+    cmake ..
+    make && make install
 
-cp ~/vimfiles/_fonts/* /Library/Fonts
+    cp ~/vimfiles/_fonts/* /Library/Fonts
 
-echo 'if [ -f ~/.bashrc ]; then . ~/.bashrc; fi' >> ~/.bash_profile
-echo 'source ~/ros_catkin_ws/install_isolated/setup.bash' >> ~/.bash_profile
-ln -s ~/Dropbox/sandbox/ ~/sandbox
-ln -s ~/Dropbox/Homework ~/hw
+    # echo 'if [ -f ~/.bashrc ]; then . ~/.bashrc; fi' >> ~/.bash_profile
+    echo 'source ~/ros_catkin_ws/install_isolated/setup.bash' >> ~/.bash_profile
 
-if [[ -d ~/.byobu ]]; then
-  mkdir ~/.byobu
+
+    ln -s ~/Dropbox/sandbox/ ~/sandbox
+    ln -s ~/Dropbox/Homework ~/hw
+
+    if [[ -d ~/.byobu ]]; then
+        mkdir ~/.byobu
+    fi
+
+    cp ~/osInstallScripts/tmux.conf ~/.byobu/.tmux.conf
+    cp ~/osInstallScripts/bash_aliases ~/.bash_aliases
+
+    echo "source ~/.bash_aliases" >> ~/.bash_profile
+    echo "source ~/.bash_profile" >> ~/.bashrc
+
+    #PX4 Install
+    # brew tap PX4/homebrew-px4
+    # brew tap osrf/simulation
+    # brew update
+    # brew install git bash-completion genromfs kconfig-frontends gcc-arm-none-eabi
+    # brew install astyle ninja
+    # brew install ant graphviz sdformat3 rotobuf
+    # brew install cgal --with-imaging
+    # pip install empy pyserial
+
+    #to mount ntfs
+    brew tap homebrew/fuse
+    brew install homebrew/fuse/osxfuse
+    brew install homebrew/fuse/ext4fuse
+
+    #Emacs BRUH!
+    brew tap d12frosted/emacs-plus
+    brew install emacs-plus
+    brew linkapps emacs-plus
+    git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
+    ln -s ~/Dropbox/spacemacs ~/.spacemacs
+    ln -s ~/Dropbox/usercfg.el ~/.emacs_usercfg.el
+
+    brew install the_silver_searcher
+    brew install ispell --with-lang-en
+    brew install ctags
+
+    mkdir ~/packages && cd packages
+    git clone https://github.com/Valloric/ycmd && cd ycmd
+    git submodule update --init --recursive
+    ./build.py --all
+    cd ~/
+
+    #Python shit
+    pip install ipython scipy flake8 autoflake scikit-learn
+
+    echo 'Whew. All done now'
+
 fi
-
-cp ~/osInstallScripts/tmux.conf ~/.byobu/.tmux.conf
-cp ~/osInstallScripts/bash_aliases ~/.bash_aliases
-echo "source ~/.bash_aliases" >> ~/.bash_profile
-
-#PX4 Install
-brew tap PX4/homebrew-px4
-brew tap osrf/simulation
-brew update
-brew install git bash-completion genromfs kconfig-frontends gcc-arm-none-eabi
-brew install astyle ninja
-brew install ant graphviz sdformat3 rotobuf
-brew install cgal --with-imaging
-pip install empy pyserial
-#to mount
-brew tap homebrew/fuse
-brew install homebrew/fuse/osxfuse
-brew install homebrew/fuse/ext4fuse
-brew install ctags
-fi
-
-echo 'Whew. All done now'
-
 #QGround Control
 #Slack
 #MenuMeters
